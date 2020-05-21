f795a2b4874f666465fc7ef4af8ce7b4e7c41a2b
Yuyang Sun <syuyangok@gmail.com>
2020-05-21 16:11:31 -0500

    Add files via upload
    Leetcode 64

---
+++ DynamicProgramming/MinimumPathSum.java
@@ -0,0 +1,32 @@
+package edu.gatech.dp;
+
+//leetcode 64, dp
+//Given a m x n grid filled with non-negative numbers,
+// find a path from top left to bottom right
+// which minimizes the sum of all numbers along its path.
+
+public class MinimumPathSum {
+
+    public static int solution(int[][] grid){
+
+        int row = grid.length;
+        int col = grid[0].length;
+
+        int[][] dp = new int[row][col];
+
+        //init base case
+        dp[0][0] = grid[0][0];
+        for(int r = 1; r< row; r++) dp[r][0] = dp[r-1][0] + grid[r][0];
+        for(int c = 1; c< col; c++) dp[0][c] = dp[0][c-1] + grid[0][c];
+
+        for(int r = 1; r< row; r++) {
+
+            for(int c = 1; c< col; c++){
+                dp[r][c] = Math.min(dp[r][c-1], dp[r-1][c]) + grid[r][c];
+            }
+        }
+        return dp[row-1][col-1];
+
+
+    }
+}
