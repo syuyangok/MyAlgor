c58f2c6c5bd5bb02744b89eb703bcf67406ed288
Yuyang Sun <syuyangok@gmail.com>
2020-07-29 10:28:34 -0500

    Write Dijkstra pesudo code

--- Graph/DijkstraPseudo.java
+++ Graph/DijkstraPseudo.java
@@ -2,4 +2,36 @@
 #n, number of nodes
 #s, the node of start
 
-function dijkstra(g, n, s)
+function dijkstra(g, n, s):
+  #create a array to record vistied nodes
+  visit[] = [false, false, false,....]; #size n
+  #create a array to record shortest value to start node
+  dist[] = [0, -1, -1, -1....];#size n
+  
+  pq; #create a priority queque   
+  pq.insert((s,0));
+  
+  while(pq not empty){    
+     (indexNode, miniValue) = pq.pop();
+     visit[indexNode] = true; #lable as visited node
+       
+    for(each edge e: g[indexNode] ){
+      
+      # if neighbor nodealready visited, skip.
+      if(visit[e.toNode] == true) continue;
+      
+      #if new value less than current value, update current value. and insert to pq.
+      if (e.value + dist[indexNode] < dist[e.toNode]){
+        dist[e.toNode] = e.value + dist[indexNode];
+        pq.insert((e.toNode, dist[e.toNode]));
+      }//#if end
+    }
+    
+  }//#while end
+
+  return dist;
+  
+
+
+  
+ 
