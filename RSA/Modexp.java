b6dfac62ac18e9a75331153c4c4564c6d79e082f
Yuyang Sun <syuyangok@gmail.com>
2020-08-06 15:11:50 -0500

    Modular exponentiation
    Modular exponentiation (Recursive)

---
+++ RSA/Modexp.java
@@ -0,0 +1,28 @@
+
+//Modular exponentiation (Recursive)
+public class Modexp {
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println(1/2);
+        int a = 7;
+        int b = 25;
+        int c = 23;
+        int result = modexp(a,b,c);
+
+        System.out.println(result);
+    }
+
+    public static int modexp(int x, int y, int N){
+
+        if(y == 0) return 1;
+
+        int z = modexp(x, y/2, N);
+        //if y is even
+        if(y%2 ==0 ) return z*z%N;
+        else return x*z*z%N;
+    }
+
+}
